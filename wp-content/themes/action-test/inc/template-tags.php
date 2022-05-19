eed2fe3b4a9b35cfb45c70dfc6e1e683f368b18e
Richard Aber <richaber@gmail.com>
2022-05-19 13:36:28 -0500

    Yet another test

--- wp-content/themes/action-test/inc/template-tags.php
+++ wp-content/themes/action-test/inc/template-tags.php
@@ -147,12 +147,12 @@ function action_test_post_thumbnail() {
 }
 
 if ( ! function_exists( 'wp_body_open' ) ) :
-    /**
-     * Shim for sites older than 5.2.
-     *
-     * @link https://core.trac.wordpress.org/ticket/12563
-     */
-    function wp_body_open() {
-        do_action( 'wp_body_open' );
-    }
+	/**
+	 * Shim for sites older than 5.2.
+	 *
+	 * @link https://core.trac.wordpress.org/ticket/12563
+	 */
+	function wp_body_open() {
+		do_action( 'wp_body_open' );
+	}
 endif;
